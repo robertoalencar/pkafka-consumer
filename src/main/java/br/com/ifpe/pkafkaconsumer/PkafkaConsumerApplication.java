a7e90d9e301ec7f93a4be5ef3f7ab42fbb275fb7
Roberto Alencar <rroberto.alencar@gmai.com>
2022-11-01 10:06:39 -0300

    enviando os arquivos do projeto criado para o git

---
+++ src/main/java/br/com/ifpe/pkafkaconsumer/PkafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.pkafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkafkaConsumerApplication.class, args);
+	}
+
+}
